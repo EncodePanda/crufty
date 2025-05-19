afc305d97e1b153a8c26df8bb7ad3494fd8c5893
EncodePanda <paul.szulc@gmail.com>
2025-05-19 22:16:10 +0200

    Add tests for scan command

--- src/crufty/cli.rs
+++ src/crufty/cli.rs
@@ -2,15 +2,33 @@ use clap::{Parser, Subcommand};
 
 /// A command-line tool that scans projects for large build artifacts and cleans
 /// them up
-#[derive(Parser)]
+#[derive(Parser, Debug, PartialEq)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
   #[command(subcommand)]
   pub command: Commands,
 }
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug, PartialEq)]
 pub enum Commands {
   /// Scan for build artifacts in the current directory
   Scan,
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_parsing_scan_command() {
+    let args = vec!["crufty", "scan"];
+    let cli = Cli::parse_from(args);
+
+    assert_eq!(
+      cli,
+      Cli {
+        command: Commands::Scan
+      }
+    );
+  }
 }
